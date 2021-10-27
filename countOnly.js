0b1187642a57810ca217082bbd1c27f1a2d1982f
Ken Cruz <ken@kencruz.ca>
2021-10-27 11:33:17 -0400

    eslinting

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -8,7 +8,7 @@ const assertEqual = function (actual, expected) {
 
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   let count = {};
   for (const item of allItems) {
     if (itemsToCount[item]) {
