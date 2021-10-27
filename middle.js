dbc32c3e964b175ec7f297bcb6998490759c81e0
Ken Cruz <ken@kencruz.ca>
2021-10-27 10:03:59 -0400

    implemented middle

---
+++ middle.js
@@ -0,0 +1,37 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const middle = (arr) => {
+  if (arr.length < 3) return [];
+
+  const mid = Math.floor(arr.length / 2);
+
+  if (arr.length % 2 === 0) {
+    return arr.slice(mid - 1, mid + 1);
+  } else {
+    return [arr[mid]];
+  }
+};
+
+// TEST CODE
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
