038336aacd67809d58fda716b4c6d8c086a32f06
Ken Cruz <ken@kencruz.ca>
2021-11-01 21:31:09 -0400

    refactored countOnly

---
+++ test/countOnlyTest.js
@@ -0,0 +1,36 @@
+const countOnly = require("../countOnly.js");
+const assert = require("chai").assert;
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+describe("#countOnly", () => {
+  it("should return 1", () => {
+    assert.strictEqual(result1["Jason"], 1);
+  });
+  it("should return undefined", () => {
+    assert.strictEqual(result1["Karima"], undefined);
+  });
+  it("should return 2", () => {
+    assert.strictEqual(result1["Fang"], 2);
+  });
+  it("should return undefined", () => {
+    assert.strictEqual(result1["Agouhanna"], undefined);
+  });
+});
