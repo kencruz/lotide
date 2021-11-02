3f0774ab162c1a98bc4d43b246f9103097416107
Ken Cruz <ken@kencruz.ca>
2021-11-01 21:02:17 -0400

    chai testing with tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,22 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
 // TEST CODE
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"//
+//assertEqual(result.length, 2); // ensure we get back two elements
+//assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+//assertEqual(result[1], "Labs"); // ensure second element is "Labs"//
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#middle", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+  it("should not modify original array in place", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+});
