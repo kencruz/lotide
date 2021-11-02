fa5d46841db2ebc52041d2f62c69e365797ddaa3
Ken Cruz <ken@kencruz.ca>
2021-11-01 21:27:00 -0400

    removed tail obsoletes

--- tail.js
+++ tail.js
@@ -1,4 +1,3 @@
-const assertEqual = require("./assertEqual");
 // FUNCTION IMPLEMENTATION
 const tail = function (arr) {
   return arr.slice(1);
--- test/tailTest.js
+++ test/tailTest.js
@@ -7,7 +7,7 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 //assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 //assertEqual(result[1], "Labs"); // ensure second element is "Labs"//
 
-describe("#middle", () => {
+describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), [
       "Lighthouse",
