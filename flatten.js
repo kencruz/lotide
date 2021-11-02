3667ad46792d4db9fd2273b2e337b4d67bdc3468
Ken Cruz <ken@kencruz.ca>
2021-11-01 21:47:41 -0400

    refactored flatten

--- flatten.js
+++ flatten.js
@@ -1,21 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
 const flatten = (arr) => {
   let output = [];
   for (let i = 0; i < arr.length; i++) {
@@ -29,6 +12,4 @@ const flatten = (arr) => {
   }
   return output;
 };
-
-// TEST CODE
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => true
+module.exports = flatten;
